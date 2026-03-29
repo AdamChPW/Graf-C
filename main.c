31b85ff7a333b8c39b95684031e9ce1f9cbb6c43
Adam Chmielewski <01206754@pw.edu.pl>
2026-03-29 12:51:28 +0000

    V1 odzczytywanie do listy sasiedztw

--- main.c
+++ main.c
@@ -1,20 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BUFSIZE 1024
 
 typedef struct l{
+    char* nazwa;    //Dodalem
     double wartosc;
     int nr_wierzcholka;
-    lista_k* next;
+    struct l* next;
 }lista_k;
 
 typedef struct m{
     int rozmiar;
-    lista_k* lista;
+    lista_k** lista;    //Zmienilem z lista* na lista **. Jak jest ktos modzejszy to niech to zmieni by dzialo z lista_k*
 }lista_sasiedztw;
 
 // Wczytuje dane do tej smiesznej macierzy / listy sasiedztw
-lista_sasiedztw* w_dane(char* f_name){
-    return NULL;
+// Ja to zrobie (Adam)
+int add_k(lista_k *origin, int to, char* nazwa, double waga)
+{
+    lista_k *k = malloc(sizeof(lista_k));
+    if( k == NULL ) return 1;
+    
+    k->nr_wierzcholka = to;
+    k->wartosc = waga;
+    k->nazwa = malloc(strlen(nazwa));
+    if( strcpy(k->nazwa, nazwa) == NULL ) return 0;
+ 
+    while(origin != NULL)
+    {
+        if(origin->nr_wierzcholka == to){
+            fprintf(stderr, "Istmieje juz droga do v%d [%s i %s]", to, nazwa, origin->nazwa);
+            return 1;
+        }
+        origin = origin->next;
+    }
+
+    origin = k;
+    return 0;
+}
+
+lista_sasiedztw* w_dane(char* f_name)
+{
+    FILE* in = fopen(f_name, "r");
+    if(in == NULL) return NULL;
+
+    lista_sasiedztw* m = malloc(sizeof(lista_sasiedztw));
+    if(m == NULL) return NULL;
+    m->rozmiar = 0;
+    m->lista = malloc(0);
+
+    char buffer[BUFSIZE];
+    while(fgets(buffer, BUFSIZE, in) != NULL){
+
+        char nazwa[BUFSIZE];
+        int v1, v2;
+        double waga;
+
+        sscanf(buffer, "%s %d %d %lf", nazwa, &v1, &v2, &waga);
+
+        if(v1 < 1 || v2 < 1){
+            fprintf(stderr, "Nr_Wierzcholka nie moze byc mniejszy od 1\n");
+            return NULL;
+        }
+        if(waga < 0){
+            fprintf(stderr, "Waga nie moze byc ujemna\n");
+            return NULL;
+        }
+
+        int new_size =  v1 < v2 && m->rozmiar < v2 ? v2 :
+                        m->rozmiar < v1 ? v1 : m->rozmiar;
+        if(new_size != m->rozmiar){    
+            m->lista = realloc(m->lista, new_size * sizeof(lista_k));
+            if(m->lista == NULL) return NULL;
+            m->rozmiar = new_size;
+        }
+        
+        if(add_k( m->lista[v1-1], v2, nazwa, waga) || add_k( m->lista[v2-1], v1, nazwa, waga)){
+            fprintf(stderr, "Wystapil blad przy dodawaniu krawedzi.\n");
+            return NULL;
+        }
+    }
+    return m;
 }
 
 // Sprawdza poprawnosc macierzy pod algorym
@@ -30,7 +98,7 @@ int wyp_dane( FILE *f ){
 }
 
 // Main. Plis zostawcie maina jak najbardziej czytelnym
-void main(int argc, char** argv)
+int main(int argc, char** argv)
 {
     if(argc < 2){
         fprintf(stderr, "Nie podano wejscie.\n");
@@ -61,3 +129,4 @@ void main(int argc, char** argv)
 
     return 0;
 }
+// Jak sie komus chce to moze to rozlorzyc na kilka pliki.
